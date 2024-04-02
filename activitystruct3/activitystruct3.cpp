bb94a32ccfa3272fa7bbbb482fdd28724ae421ec
rcslbaa <ifasmarani3125@gmail.com>
2024-04-02 18:11:23 +0700

    commit 13

--- activitystruct3/activitystruct3.cpp
+++ activitystruct3/activitystruct3.cpp
@@ -13,3 +13,10 @@ struct mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs;
+	cout << "Nomor mahasiswa : ";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
