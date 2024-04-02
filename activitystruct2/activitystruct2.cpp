29462226e5573fb566261c9a8ed07df98274070f
rcslbaa <ifasmarani3125@gmail.com>
2024-04-02 18:04:12 +0700

    commit 8

--- activitystruct2/activitystruct2.cpp
+++ activitystruct2/activitystruct2.cpp
@@ -13,3 +13,16 @@ struct Mahasiswa {
     DetailAlamat alamat;
     int umur;
 };
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
