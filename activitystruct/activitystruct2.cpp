0683ce4102da743fbce211e54e34030b2a409e2e
rcslbaa <ifasmarani3125@gmail.com>
2024-04-02 17:11:00 +0700

    struct mahasiswa

---
+++ activitystruct/activitystruct2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+	
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	int umur;
+};
