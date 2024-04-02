b87dcad284936f13d20bfdda3481755d4fa891eb
rcslbaa <ifasmarani3125@gmail.com>
2024-04-02 19:22:39 +0700

    commit 18

--- activitystruct_4/activitystruct_4.cpp
+++ activitystruct_4/activitystruct_4.cpp
@@ -13,3 +13,12 @@ struct Mahasiswa {
     DetailAlamat alamat;
     int umur;
 };
+
+int main() {
+    Mahasiswa mhs[3];
+    for (int i = 0; i < 3; i++) {
+        cout << "Data ke-" << (i + 1) << ":" << endl;
+        cout << "Nomor Mahasiswa : ";
+        getline(cin, mhs[i].nim);
+        cout << "Nama Mahasiswa : ";
+        getline(cin, mhs[i].nama);
